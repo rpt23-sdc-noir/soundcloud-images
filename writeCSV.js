069f47bdc84ed70ff23016537051df89c062bb9b
redperezz <kaicruzz91@gmail.com>
2020-12-05 12:31:19 -0800

    Refactored writeCsv function to include song name property

--- writeCSV.js
+++ writeCSV.js
@@ -5,6 +5,13 @@ const faker = require('faker');
 const bands = require('./bandData.js');
 
 const getRandomNum = max => Math.floor(Math.random() * Math.floor(max));
+const getSongName = () => {
+  const getSongName = faker.lorem.words();
+  const splitSongName = getSongName.split(' ');
+  const capFirstLetters = splitSongName.map(word => word = word.charAt(0).toUpperCase() + word.substr(1));
+  const joinSongName = capFirstLetters.join(' ');
+  return joinSongName;
+}
 
 const writeToCsvFile = async () => {
   writer.pipe(fs.createWriteStream('data.csv'));
@@ -12,10 +19,11 @@ const writeToCsvFile = async () => {
    await writer.write({
       band_id: i + 1,
       song_id: i + 1,
-      band_image_url: bands.bandImages[getRandomNum(150)],
       band_name: faker.vehicle.vehicle(),
+      song_name: getSongName(),
       followers: getRandomNum(10000000),
-      tracks: getRandomNum(2500000)
+      tracks: getRandomNum(2500000),
+      band_image_url: bands.bandImages[getRandomNum(150)]
     })
   }
   writer.end();
