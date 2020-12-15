71689a74f27345010dd207a184d025e42dbf0651
redperezz <kaicruzz91@gmail.com>
2020-12-15 12:22:55 -0800

    New Relic & k6

---
+++ script.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2m', target: 100 },
+    { duration: '5m', target: 100 },
+    { duration: '2m', target: 200 },
+    { duration: '5m', target: 200 },
+    { duration: '2m', target: 300 },
+    { duration: '5m', target: 300 },
+    { duration: '2m', target: 400 },
+    { duration: '5m', target: 400 },
+    { duration: '10m', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};  
+
+export default function () {
+  const randomId = Math.floor(Math.random() * 10000000);
+  const res = http.get(`http://127.0.0.1:2000/bands/get/${randomId}`);
+  check(res, {'status was 200': (r) => r.status == 200});
+  sleep(1);
+};
+
+
